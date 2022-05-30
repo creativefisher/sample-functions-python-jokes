88571570e8747987ced16a2caa17d29eca526bf9
creativefisher <57014955+creativefisher@users.noreply.github.com>
2022-05-29 21:40:42 -0700

    Update __main__.py

--- packages/default/joke/__main__.py
+++ packages/default/joke/__main__.py
@@ -1,7 +1,9 @@
 import pyjokes
 
 def main(args):
-  joke = pyjokes.get_joke()
+  joke1 = pyjokes.get_joke()
+  joke2 = pyjokes.get_joke()
+  joke = joke1 + "\n" +joke2
   return {
     'body': {
       'response_type': 'in_channel',
